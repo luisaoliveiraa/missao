bfeeffe2b307f43093ffaf7301a27836308d9995
luisaoliveiraa <105437774+luisaoliveiraa@users.noreply.github.com>
2024-08-30 07:56:53 -0300

    ok

--- script.js
+++ script.js
@@ -47,3 +47,39 @@ const perguntas = [
         ]
     }
 ];
+let atual = 0;
+let perguntaAtual;
+let historiaFinal = "";
+function mostraPergunta () {
+ if (atual >= perguntas.length) {
+ mostraResultado();
+ return;
+ }
+perguntaAtual = perguntas [atual];
+caixaPerguntas.textContent = perguntaAtual.enunciado; 
+caixaAlternativas.textContent = ""; 
+textoResultado.textContent
+mostraAlternativas ();
+
+}
+function mostraAlternativas () {
+ for (const alternativa of perguntaAtual.alternativas) 
+    { const botaoAlternativa = document.createElement("button"); 
+    botaoAlternativa.textContent = alternativa.texto;
+    botaoAlternativa.addEventListener("click", () =>
+respostaSelecionada (alternativa));
+    caixaAlternativas.appendChild (botaoAlternativa);
+   }
+}
+function respostaSelecionada (opcaoSelecionada) { 
+    const afirmacao = opcaoSelecionada.afirmacao; 
+    historiaFinal += afirmacao + " "; 
+    atual++
+    mostraPergunta ();
+}
+function mostraResultado (){
+    caixaPerguntas.textContent = "Resumindo...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
+}
+     mostraResultado();
